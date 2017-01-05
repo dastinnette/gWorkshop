e5a7cb619c7b5f1ce572d39f6444b18768aa2b50
David Stinnette <dastinnette@gmail.com>
2017-01-04 18:45:19 -0700

    Added googlemapsAPI project

---
+++ 7week/2class/googleMapsAPI/main.js
@@ -0,0 +1,10 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -13.1631412,
+      lng: -72.5471516
+    },
+    zoom: 14
+  });
+
+}
